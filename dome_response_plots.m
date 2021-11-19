920919044b476ae6bad0dce8aafff8729e85b42a
suh434 <70988506+suh434@users.noreply.github.com>
2021-11-19 17:15:02 -0500

    Add files via upload

---
+++ dome_response_plots.m
@@ -0,0 +1,18 @@
+%% Plots
+% This mfile gets the "Lin_sys" from "dome" or "dome_train_test" and
+% plots impulse and pulse response of the identified system.
+
+% Impulse Response
+figure (1)
+[y,t] = impulse(Lin_sys);
+plot(t,y,'color','[0.9290, 0.6940, 0.1250]','LineWidth', 2)
+title('System Impulse Response');xlabel('Time (minute)'); ylabel('Impulse Response');
+
+% Pulse Response
+figure (2)
+stim = [ones(10,1); zeros(length(y)-10, 1)];  % considering 10 pulses    
+h = tril(toeplitz(y));
+Pulsei= h*stim;
+plot (t, Pulsei,'color','[0.9290, 0.6940, 0.1250]','LineWidth', 2);
+title('System Pulse Response');
+xlabel('Time (minute)'); ylabel('Pulse Response');
